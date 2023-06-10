43cee2fe52a7bec393e0aa7d737153c9be4ad888
Gregory <gregory.ovis@gmail.com>
2023-06-10 10:48:47 -0400

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,18 +16,26 @@ function App() {
     <div className="App">
       <h1 className="titleHeading">JustSing</h1>
 
-      <input
-        className="inp"
-        type="text"
-        placeholder="Song name"
-        onChange={(e) => {
-          setSong(e.target.value);
-        }}
-      />
 
-      <button className="btn" onClick={() => search(song)}>
-        ???? Search
-      </button>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input
+          className="inp"
+          type="text"
+          placeholder="Song name"
+          onChange={(e) => {
+            setSong(e.target.value);
+          }}
+          class = 'block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blurple focus:border-blurple'
+        ></input>
+
+
+        <button className="btn" class="text-white absolute right-2.5 bottom-2.5 bg-blurple hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blurple font-medium rounded-lg text-sm px-4 py-2" onClick={() => search(song)}>
+          ???? Search
+        </button>
+      </div>
 
       <div className="song-list flex-col	flex">
         {songs.map((_song, i) => {
@@ -40,12 +48,12 @@ function App() {
               <div className="flex flex-row content-center	 items-center ml-2">
                 <p> {_song.name} </p>
               </div>
-              <div className=" flex flex-row justify-between	w-80  items-center mr-4 text-sm	">
-                <button class="	text-white bg-blight  justify-evenly hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-2 py-2 text-center mr-3 md:mr-0 w-smallx flex 	 items-center" onClick={() => setTheSong(_song, true)}>
+              <div className=" flex flex-row justify-between	w-full  items-center mr-4 text-sm	">
+                <button class="	text-white bg-blight  justify-between hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blurple font-medium rounded-lg text-xs px-2 py-2 text-center mr-3 md:mr-0 w-60 flex 	 items-center mx-8 px-5" onClick={() => setTheSong(_song, true)}>
                    <BiUserVoice />
-                  With Vocals
+                 With Vocals
                 </button>
-                <button class="text-white bg-blight justify-evenly hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-2 py-2 text-center mr-3 md:mr-0  w-40   flex items-center  " onClick={() => setTheSong(_song, false)}>
+                <button class="text-white bg-blight justify-evenly hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blurple font-medium rounded-lg text-xs px-2 py-2 text-center mr-3 md:mr-0  w-60   flex items-center mx-8 px-5" onClick={() => setTheSong(_song, false)}>
                  <BsPerson />
                   Without Vocals
                 </button>
