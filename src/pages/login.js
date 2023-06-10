aa9fc635ce223bcaea4707e51b0587ae0c8617d7
Fiona Cai <fiona.cai899@gmail.com>
2023-06-10 17:24:37 -0400

    some changes

--- src/pages/login.js
+++ src/pages/login.js
@@ -32,25 +32,23 @@ export default function Login() {
     }
   
     return (
-      <div className="App bg-dark h-screen h-60 flex items-center justify-center">
-        <div class="mx-2 text-center">
-          <h1 className="title" class='text-white font-extrabold text-4xl xs:text-5xl md:text-6xl'>Username</h1>
-        </div>
-        <div className=" absolute top-[417px] left-[200px] w-[724px] h-[77px] pl-[55px] pr-[480px] pt-[22px] pb-[21px] border-[5px] border-white border-solid rounded-[50px] box-border">
-          <index 
-            type="text"
-            placeholder="Username"
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          className=" inp border-[#ffffffff] text-2xl leading-6  font-syne  font-[700]">
-          </index>
-        </div>
-        <div className=" absolute top-[417px] left-[955px] w-[125px] h-[77px] px-[29px] pt-[18px] pb-[17px] border-[5px] border-white border-solid rounded-[50px] box-border  bg-[rgba(105,46,231,1)]">
-          <button className="btn flex flex-col justify-center  border-[#ffffffff]  text-[32px] leading-8  font-syne  font-[700] text-center" onClick={() => loginUser(username)}>
-            GO
-          </button>
+      <div className="App">
+        <h1 className="titleHeading">Login</h1>
+
+        <div className="song-list flex-col	flex">
+            {playerInfo.map((player, i) => {
+                return (
+                    <div
+                        className="flex  flex-row	 justify-between	 border-2 border-purple hover:bg-gray-light hover:bg-slate-400	 mt-2 h-12 rounded-lg "
+                        key={i["username"]}
+                        >
+                        <div className="flex flex-row content-center	 items-center ml-2">
+                            <p> {i["username"]} </p>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
       </div>
-    )
+    );
   }
