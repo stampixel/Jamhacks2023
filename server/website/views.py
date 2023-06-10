55c7c2792f9517abac02d84bd2b9fc1ea48b98a4
Arihan10 <sofwarearihan@gmail.com>
2023-06-09 23:43:50 -0400

    added second user functionality

--- server/website/views.py
+++ server/website/views.py
@@ -5,27 +5,31 @@ from flask import Blueprint, render_template, redirect, request, flash, url_for,
 import random
 from . import app
 import os
+from . import db
 
 views = Blueprint('views', __name__)
 
+users = db.Users; 
 
-# Index page
-@views.route('/profile')
-def profile():
-
-    response_body = {
-        "name": "Kevin",
-        "about": "Hello! I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
+username = ""; 
 
 
-@views.route('/receive', methods=['GET', 'POST'])
+@views.route('/user', methods=['GET', 'POST'])
 def data():
     if request.method == 'POST':
-        print("asd")
         data = request.get_data()
         print(data)
+
+        user = users.find_one({"username": data.username})
+
+        if (user): 
+            username = user.username; 
+        else: 
+            users.insert_one({
+                "username": data.username, 
+                "word_accuracy": 0, 
+                "pitch_accuracy": 0,
+                "total_score": 0
+            })
 
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
