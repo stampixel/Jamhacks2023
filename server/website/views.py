371370c9e46c6470d7835d805b95fc08c6203a6d
bourrasque <stampixel.t@gmail.com>
2023-06-10 03:09:45 -0400

    downloading file

--- server/website/views.py
+++ server/website/views.py
@@ -8,25 +8,20 @@ import os
 from . import db
 import youtube_dl
 
-YTDL_OPTIONS = {
-    'format': 'bestaudio/best',
-    'extractaudio': True,
-    'audioformat': 'mp3',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'ytsearch',
-    'source_address': '0.0.0.0',
-}
+from requests import get
+# from youtube_dl import YoutubeDL
+
+import yt_dlp as youtube_dl
+from yt_dlp import YoutubeDL
+from youtube_search import YoutubeSearch
+
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True', 'postprocessors': [{
+    'key': 'FFmpegExtractAudio',
+    'preferredcodec': 'mp3',
+    'preferredquality': '192',
+}]}
 
 views = Blueprint('views', __name__)
-ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
-
 
 users = db.Users
 
@@ -47,10 +42,9 @@ def test_profile():
 @views.route('/receive', methods=['GET', 'POST'])
 def test_data():
     if request.method == 'POST':
-        print("asd")
         data = request.get_data()
-        print(data)
-
+        print(data["name"])
+        download_video(data["name"])
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
@@ -77,11 +71,36 @@ def login():
 @views.route('/music_json', methods=['GET', 'POST'])
 def music_json():
     if request.method == 'POST':
-        data = request.get_data()
+        data = json.loads(request.get_data())
         print(data)
+        print(str(data["name"]) + " " + str(data["lines"][-1]["timeTag"]))
+
+        download_video(data["name"], data["length"])
 
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
+
+def download_video(song, time_length):
+    results = YoutubeSearch(song, max_results=10).to_dict()
+
+    for data in results:
+        if len(data['duration'].split(":")) < 3:
+            print(data["duration"].split(":"))
+            time = (int(data["duration"].split(":")[0]) * 60) + int(data["duration"].split(":")[1])
+            print(time, time_length)
+            if abs(int(time) - int(time_length)) < 3:
+
+                title = data["title"]
+                with YoutubeDL(YDL_OPTIONS) as ydl:
+                    try:
+                        get(title)
+                    except:
+                        video = ydl.extract_info(f"ytsearch:{title}", download=True)['entries'][0]
+                    else:
+                        video = ydl.extract_info(title, download=True)
+
+                    print(video["duration"])
+                break
 @views.route('/lyric_data', methods=['GET'])
 def process_lyric():
     data = request.get_data()
