5b405a9a52a55f741ed940c7be3b2f4e566cf582
Fiona Cai <fiona.cai899@gmail.com>
2023-06-10 17:42:05 -0400

    ewfonts

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -9,7 +9,7 @@ export default function Landing() {
           <a href="https://flowbite.com/" class="flex items-center">
               <img src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/r8cwveuu3z-8%3A9?alt=media&token=1bf4f878-9242-4cf5-a61f-576db78653a3" class="h-8 mr-3" alt="Flowbite Logo">
               </img>
-              <span class="self-center text-2xl font-semibold whitespace-nowrap">Just Sing</span>
+              <span class="self-center text-2xl font-semibold whitespace-nowrap font-syne">Just Sing</span>
           </a>
           <div class="flex md:order-2">
               <button type="button" class="text-white bg-blight hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Get started</button>
