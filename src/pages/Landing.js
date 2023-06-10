78e14d4853240348092cb7a3c84b19422f3f2070
Fiona Cai <fiona.cai899@gmail.com>
2023-06-10 14:56:16 -0400

    all your favs

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,12 +4,12 @@ export default function Landing() {
   return (
     <div className="bg-dark h-full w-full z-0">
     
-      <nav class="bg-dark fixed w-full z-20 top-0 left-0 text-white">
+      <nav class="bg-dark w-full z-20 top-0 left-0 text-white">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" class="flex items-center">
               <img src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/r8cwveuu3z-8%3A9?alt=media&token=1bf4f878-9242-4cf5-a61f-576db78653a3" class="h-8 mr-3" alt="Flowbite Logo">
               </img>
-              <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+              <span class="self-center text-2xl font-semibold whitespace-nowrap">Just Sing</span>
           </a>
           <div class="flex md:order-2">
               <button type="button" class="text-white bg-blight hover:bg-blurple focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Get started</button>
