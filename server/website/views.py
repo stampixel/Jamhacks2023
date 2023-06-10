558489dd56aa7f9ba06ec6538cd6ccbecebc0259
bourrasque <stampixel.t@gmail.com>
2023-06-09 23:52:42 -0400

    music_json

--- server/website/views.py
+++ server/website/views.py
@@ -11,16 +11,25 @@ views = Blueprint('views', __name__)
 
 users = db.Users; 
 
-# Index page
+# THIS IS A TEST METHOD
 @views.route('/profile')
 def profile():
-
     response_body = {
         "name": "Kevin",
         "about": "Hello! I'm a full stack developer that loves python and javascript"
     }
 
     return response_body
+
+# THIS IS A TEST METHOD
+@views.route('/receive', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        print("asd")
+        data = request.get_data()
+        print(data)
+
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @views.route('/receive', methods=['GET', 'POST'])
@@ -40,5 +49,14 @@ def data():
                 "pitch_accuracy": 0,
                 "total_score": 0
             })
+
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@views.route('/music_json', methods=['GET', 'POST'])
+def music_json():
+    if request.method == 'POST':
+        data = request.get_data()
+        print(data)
 
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
